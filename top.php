ee84685637bf1960fd405bbabb357aca9cdb9fb1
Ishak Dervisevic <email@ishakdervisevic.com>
2021-05-11 15:53:30 +0200

    Added favicon

--- top.php
+++ top.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cool Place online</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="media/atom.gif" type="image/gif" >
 </head>
 
 <body>
