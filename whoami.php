a07b9b7290076e226a4e6e2066b31574d5783858
Ishak Dervisevic <email@ishakdervisevic.com>
2021-06-11 15:15:19 +0200

    Removed thundercock gif :(

--- whoami.php
+++ whoami.php
@@ -2,7 +2,6 @@
 <center style="margin-top: 30vh;">
     <div style="max-width: 800px; background-color: greenyellow; padding: 15px; border: dotted black 5px ; margin: 5px">
     <h1>I AM THUNDERCOCK!</h1>
-    <img style="width: 200px; height: auto;" src="media/thundercock.gif" alt="penis img">
     </div>
 </center>
 <?php include("bottom.php");?>
