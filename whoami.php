7bb627aa07b0e635f1da71a3ffbd5547f11ba71a
Ishak Dervisevic <email@ishakdervisevic.com>
2021-06-11 15:06:12 +0200

    Thundercock added

--- whoami.php
+++ whoami.php
@@ -1,10 +1,8 @@
 <?php include("top.php");?>
 <center style="margin-top: 30vh;">
     <div style="max-width: 800px; background-color: greenyellow; padding: 15px; border: dotted black 5px ; margin: 5px">
-    <h1>DOESN'T FUCKING MATTER, DUMBASS</h1>
-    <h2>PISS OFF</h2>
-    <h4>xD</h4>
-    <h6>smh</h6>
+    <h1>I AM THUNDERCOCK!</h1>
+    <img style="width: 200px; height: auto;" src="media/thundercock.gif" alt="penis img">
     </div>
 </center>
 <?php include("bottom.php");?>
